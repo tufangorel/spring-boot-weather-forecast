5cb59c99d76b0099fbb206013dce3dab3ffe76ad
tufangorel <tufang@gmail.com>
2023-10-31 19:41:06 +0300

    refactor

--- spring-boot-weather-forecast-client-api/src/main/java/com/weather/forecast/api/util/ApplicationConstants.java
+++
@@ -1,8 +0,0 @@
-package com.weather.forecast.api.util;
-
-public class ApplicationConstants {
-
-    public static final String REQUEST_ID_HEADER = "request-id";
-    public static final String CORRELATION_ID_HEADER = "correlation-id";
-    public static final String APPLICATION_REQUEST_CONTEXT_DATA_KEY = "applicationRequestContextData";
-}
