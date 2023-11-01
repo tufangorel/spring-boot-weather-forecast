3b88b1e53c4572dad4be713c8e06bbad0ebf4b9c
tufangorel <tufang@gmail.com>
2023-11-02 00:58:30 +0300

    Add more null control checks

--- spring-boot-weather-forecast-client-api/src/main/java/com/weather/forecast/api/controller/WeatherForecastController.java
+++ spring-boot-weather-forecast-client-api/src/main/java/com/weather/forecast/api/controller/WeatherForecastController.java
@@ -1,6 +1,5 @@
 package com.weather.forecast.api.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.weather.forecast.api.model.request.City;
 import com.weather.forecast.api.model.response.WeatherForecastResponse;
 import com.weather.forecast.api.service.WeatherForecastService;
@@ -20,7 +19,6 @@ public class WeatherForecastController {
 
     private static final Logger Log = LoggerFactory.getLogger(WeatherForecastController.class);
 
-
     private final WeatherForecastService weatherForecastService;
 
     public WeatherForecastController(WeatherForecastService weatherForecastService) {
@@ -31,57 +29,74 @@ public class WeatherForecastController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved maxFeelsLike and maxHumidity"),
             @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
-            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")
+            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found"),
+            @ApiResponse(responseCode = "500", description = "City value can not be null")
     })
     @PostMapping("/by-city-id")
-    public ResponseEntity<WeatherForecastResponse> getWeatherForecastByCityId(@RequestBody City city) throws JsonProcessingException {
+    public ResponseEntity<?> getWeatherForecastByCityId(@RequestBody City city) throws Exception {
 
-        Long cityId = city.getCityId();
-        if( cityId != null ) {
+        if( city != null && city.getCityId() != null ) {
+            Long cityId = city.getCityId();
             Log.info("Fetching weather details with cityId: {}", cityId);
             WeatherForecastResponse weatherForecastResponse = weatherForecastService.getWeatherForecastByCityId(cityId);
             if (weatherForecastResponse != null) {
                 return new ResponseEntity<>(weatherForecastResponse, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         }
 
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<String>("City value can not be null", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @Operation(summary = "Get Weather Forecast By City Id and access token")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved maxFeelsLike and maxHumidity"),
             @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
-            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")
+            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found"),
+            @ApiResponse(responseCode = "500", description = "City value and appId can not be null")
     })
     @GetMapping("/get/{cityId}/{appId}")
-    public ResponseEntity<WeatherForecastResponse> getWeatherForecastByCityIdAndAppID( @PathVariable("cityId") String cityId,
-                                                              @PathVariable("appId") String appId) throws JsonProcessingException {
+    public ResponseEntity<?> getWeatherForecastByCityIdAndAppID( @PathVariable("cityId") String cityId,
+                                                              @PathVariable("appId") String appId) throws Exception {
 
-        Log.info("Fetching weather details with cityId: {}", cityId);
+        if( cityId != null && appId != null ) {
+            Log.info("Fetching weather details with cityId: {}", cityId);
+            WeatherForecastResponse weatherForecastResponse = weatherForecastService.getWeatherForecastByCityIdAndAppID(cityId, appId);
 
-        WeatherForecastResponse weatherForecastResponse = weatherForecastService.getWeatherForecastByCityIdAndAppID(cityId, appId);
-
-        if (weatherForecastResponse != null) {
-            return new ResponseEntity<>(weatherForecastResponse, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            if (weatherForecastResponse != null) {
+                return new ResponseEntity<>(weatherForecastResponse, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         }
+
+        return new ResponseEntity<String>("City value and appId can not be null", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Operation(summary = "Get Weather Forecast By City Id and access token in http request header")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved maxFeelsLike and maxHumidity"),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found"),
+            @ApiResponse(responseCode = "500", description = "City value and authorizationHeader can not be null")
+    })
     @GetMapping(path = "/get/{cityId}")
-    public ResponseEntity<WeatherForecastResponse> getWeatherForecastByCityIdAndAppIDWithHeader( @PathVariable("cityId") String cityId,
-                                                                        @RequestHeader(value="Authorization") String authorizationHeader ) throws JsonProcessingException {
-        Log.info("Fetching weather details with cityId: {}", cityId);
-        String appId = authorizationHeader;
-        WeatherForecastResponse weatherForecastResponse = weatherForecastService.getWeatherForecastByCityIdAndAppID(cityId, appId);
+    public ResponseEntity<?> getWeatherForecastByCityIdAndAppIDWithHeader( @PathVariable("cityId") String cityId,
+                                                                        @RequestHeader(value="Authorization") String authorizationHeader ) throws Exception {
 
-        if (weatherForecastResponse != null) {
-            return new ResponseEntity<>(weatherForecastResponse, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if( cityId != null && authorizationHeader != null ) {
+            Log.info("Fetching weather details with cityId: {}", cityId);
+            WeatherForecastResponse weatherForecastResponse = weatherForecastService.getWeatherForecastByCityIdAndAppID(cityId, authorizationHeader);
+
+            if (weatherForecastResponse != null) {
+                return new ResponseEntity<>(weatherForecastResponse, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         }
 
+        return new ResponseEntity<String>("City value and authorizationHeader can not be null", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
