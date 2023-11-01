467f79aef5b8e2d26bdcccdfbad5123c529aa848
tufangorel <tufang@gmail.com>
2023-11-01 12:51:09 +0300

    remove mapstruct dependency

--- spring-boot-weather-forecast-client-api/src/main/java/com/weather/forecast/api/controller/WeatherForecastController.java
+++ spring-boot-weather-forecast-client-api/src/main/java/com/weather/forecast/api/controller/WeatherForecastController.java
@@ -1,10 +1,9 @@
 package com.weather.forecast.api.controller;
 
-import com.weather.forecast.api.model.request.City;
 import com.weather.forecast.api.model.WeatherResponse;
+import com.weather.forecast.api.model.request.City;
 import com.weather.forecast.api.service.WeatherForecastService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
@@ -28,7 +27,6 @@ public class WeatherForecastController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved list"),
             @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
-            @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")
     })
     @PostMapping("/by-city-id")
